785557e72b5ae17eb813556277ffa59750da9bc2
Zubairahmdfaraz <96882601+Zubairahmdfaraz@users.noreply.github.com>
2024-02-28 16:32:07 +0530

    Update test_AddEmployees.py

--- testCases/test_AddEmployees.py
+++ testCases/test_AddEmployees.py
@@ -36,7 +36,7 @@ class addEmployees(unittest.TestCase):
 
     def setUp(self):
         self.logger = LogGen.loggen()
-        self.driver = webdriver.Chrome()  # Change to the appropriate driver
+        self.driver = webdriver.Firefox()  # Change to the appropriate driver
         self.driver.maximize_window()
         self.driver.implicitly_wait(10)
         self.logger.info("****Opening URL****")
